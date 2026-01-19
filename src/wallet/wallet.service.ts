3349f199260281f6b70da1e49b8e27dc863fa140
Angelitoo777 <angeloropeza1604@gmail.com>
2026-01-19 14:39:51 -0400

    feature: se agrego la mayoria de lo necesario del modulo de wallets y a la vez se implemento las transaciones en los modulos de users y auth para poder cordinar bien las wallets.'

--- src/wallet/wallet.service.ts
+++ src/wallet/wallet.service.ts
@@ -1,26 +1,55 @@
 import { Injectable } from '@nestjs/common';
-import { CreateWalletDto } from './dto/create-wallet.dto';
-import { UpdateWalletDto } from './dto/update-wallet.dto';
+import { WalletRepository } from './wallet.repository';
+import { WalletTransactionRepository } from './walletTransactions.repository';
+import { Sequelize } from 'sequelize-typescript';
+import { Transaction } from 'sequelize';
 
 @Injectable()
 export class WalletService {
-  create(createWalletDto: CreateWalletDto) {
-    return 'This action adds a new wallet';
+  constructor(
+    private sequelize: Sequelize,
+    private walletRepository: WalletRepository,
+    private transactionsRepository: WalletTransactionRepository,
+  ) {}
+
+  async deposit(userId: string | undefined, amount: number) {
+    const t = await this.sequelize.transaction();
+
+    if (!userId) {
+      throw new Error('User ID es requerido para el deposito');
+    }
+
+    try {
+      const wallet = await this.walletRepository.findByUserId(userId);
+
+      if (!wallet) {
+        throw new Error('Cartera no encontrada para el usuario');
+      }
+
+      const newBalance = Number(wallet?.balance) + amount;
+
+      await this.walletRepository.updateBalance(wallet?.id, newBalance, t);
+
+      await this.transactionsRepository.create(
+        {
+          wallet_id: wallet?.id,
+          amount,
+          type: 'deposit',
+          reference_id: 'external',
+        },
+        t,
+      );
+
+      await t.commit();
+
+      return { message: 'Deposito exitoso', newBalance };
+    } catch (error) {
+      await t.rollback();
+      throw error;
+    }
   }
 
-  findAll() {
-    return `This action returns all wallet`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} wallet`;
-  }
-
-  update(id: number, updateWalletDto: UpdateWalletDto) {
-    return `This action updates a #${id} wallet`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} wallet`;
+  async createWallet(userId: string, t: Transaction) {
+    return this.walletRepository.create(userId, t);
   }
 }
